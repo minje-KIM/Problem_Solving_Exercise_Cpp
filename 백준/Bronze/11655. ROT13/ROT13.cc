f891f8333530d0251c0643b3eea5b474fcd40ab2
minje KIM <minje617@naver.com>
2024-07-22 14:54:22 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// 띄어쓰기를 포함한 문자열 => getline
 string s;
+int cnt[26];
+int n;
+string ret;
 
 int main ()
 {
-    getline(cin, s);
+    getline(cin , s);
     
-    for (int i=0; i<s.size(); i++)
+    
+    for (int i=0; i< s.size(); i++)
     {
-        // 대문자
-        if (s[i] >= 65 && s[i] <=90)
+        if (s[i] >= 65 && s[i] < 91)
         {
             if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
-            else s[i] += 13;
+            else s[i] = s[i] + 13;
         }
-        // 소문자
-        else if (s[i] >= 97 && s[i] <= 122)
+        else if (s[i] >= 97 && s[i] < 123)
         {
             if (s[i] + 13 > 122) s[i] = s[i] + 13 - 26;
-            else s[i] += 13;
+            else s[i] = s[i] + 13;
         }
-        else continue;
+        
+        cout << s[i];
     }
     
-    cout << s;
+
     return 0;
 }
+
+
+
