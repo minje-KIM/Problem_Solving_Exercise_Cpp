b90f63031de8e140ecd6b44a0e9eff066a0e7daf
minje KIM <minje617@naver.com>
2024-07-22 11:00:28 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,35 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt[104];
 int a, b, c;
-int s, e;
+int cnt[105];
+int x, y;
 int ret;
-
-// 시작한 시간 / 끝난 시간 -> 이상 / 미만 으로 설정
-// 카운팅 배열이 필요하다
 
 int main ()
 {
     cin >> a >> b >> c;
     
-    for (int i=0; i<3; i++)
+    for (int i=0; i < 3; i ++)
     {
-        cin >> s >> e;
-        for (int j=s; j<e; j++)
+        cin >> x >> y;
+        
+        for (int j=x; j<y; j++)
             cnt[j]++;
     }
-    
-    for (int i=0; i<100; i++)
+
+    for (int i=1; i<100; i++)
     {
-        if (cnt[i] == 1) ret +=(1*a);
+        if (cnt[i] == 1) ret += a;
         else if (cnt[i] == 2) ret += (2*b);
         else if (cnt[i] == 3) ret += (3*c);
-        else continue;
-    }
+    }   
     
-    cout << ret;
+    cout << ret << "\n";
+    
     return 0;
-    
-    
 }
+
+
+
