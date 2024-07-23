43dadce699ba85d7da98ed4eb8d949dbe05356b8
minje KIM <minje617@naver.com>
2024-07-23 10:09:59 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s, ret;
+int cnt[200];
+int flag;
+char mid;
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> s;
+    
+    for (char c : s) cnt[c]++;
+    for (int i='Z'; i>='A'; i--)
+    {
+        if(cnt[i])
+        {
+            if (cnt[i] % 2 == 1)
+            {
+                flag++;
+                mid = char(i);
+                cnt[i]--;
+            }
+            
+            if (flag == 2) break;
+            
+            for (int j=0; j< cnt[i]; j+=2)
+            {
+                ret = char(i) + ret;
+                ret += char(i);
+            }
+        }    
+    }
+    
+    if(mid) ret.insert(ret.begin() + ret.size()/2, mid);
+	if(flag == 2)cout << "I'm Sorry Hansoo\n";
+    else cout << ret << "\n";    
+}
+
+
