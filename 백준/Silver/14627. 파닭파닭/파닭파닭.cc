f642b22361884ab47cf922a38dd992313e3f881d
minje KIM <minje617@naver.com>
2024-09-19 17:22:14 +0900

    [Silver II] Title: 파닭파닭, Time: 780 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll s, c;
+ll chk[1000004];
+ll lo, hi;
+ll maxi;
+ll ret;
+ll sum;
+ll mid;
+
+
+bool check(ll mid)
+{
+    ll tmp = 0;
+    for (int i=0; i<s; i++)
+    {
+        tmp += chk[i] / mid;
+    }
+    
+    return (tmp >= c);
+}
+
+
+int main ()
+{
+    cin >> s >> c;
+    
+    for (int i=0; i<s; i++)
+    {
+        cin >> chk[i];
+        
+        sum += chk[i];
+        
+        if (chk[i] > maxi)
+            maxi = chk[i];
+    }
+    
+    lo = 1;
+    hi = maxi;
+    
+    while (lo <= hi)
+    {
+        mid = (lo + hi) / (1LL * 2) ;
+
+        if (check(mid))
+        {
+            lo = mid + 1;
+            ret = mid;
+        }
+        else
+        {
+            hi = mid - 1;
+        }
+        
+    }
+    
+    cout << sum - (ret * c) << "\n";
+    
+    
+    return 0;
+    
+}
