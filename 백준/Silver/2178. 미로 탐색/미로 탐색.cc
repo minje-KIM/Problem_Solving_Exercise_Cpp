9ccdf3ad72949a9c04666ac44e1479a228937b05
minje KIM <minje617@naver.com>
2025-01-02 20:15:59 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,40 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string temp = "";
 
-// 최대 정수의 크기가 100 -> 그보다 크게 104
-int miro[104][104];
-int visited[104][104];
-
-int N;
-int M;
-
+int n, m;
+const int max_n = 104;
+int visited[max_n][max_n];
+int a[max_n][max_n];
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
 int y, x;
-
-const int dy[] = {0, -1, 0, 1};
-const int dx[] = {1, 0, -1, 0};
 
 
 int main ()
 {
-    //최소의 칸 수 -> 최단경로 문제 -> BFS로 풀이
-    // 지도 기반의 입력 -> 지도를 만들어 풀이
+    scanf("%d %d", &n, &m); 
     
-    //붙어서 들어오는 입력 -> string 으로 처리
-    
-    cin >> N >> M;
-    
-    for (int i=0; i<N; i++)
+    for(int i = 0; i < n; i++)
     {
-        cin >> temp;
-        for (int j=0; j<M; j++)
+        for(int j = 0; j < m; j++)
         {
-            miro[i][j] = temp[j] - '0';
+            scanf("%1d", &a[i][j]);
         }
-    }
-    
-    // bfs 구현
+    } 
     
     queue<pair<int, int>> q;
     visited[0][0] = 1;
@@ -42,24 +29,23 @@ int main ()
     
     while(q.size())
     {
-        tie(y, x) = q.front(); 
+        tie(y,x) = q.front();
         q.pop();
         
-        for (int i=0; i<4; i++)
+        for (int i=0; i< 4; i++)
         {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            
-            if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-            if (miro[ny][nx] == 0) continue;
+        
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
             if (visited[ny][nx]) continue;
-            
+            if (a[ny][nx] == 0) continue;
             visited[ny][nx] = visited[y][x] + 1;
             q.push({ny, nx});
-            
         }
     }
     
-    cout << visited[N-1][M-1];    
+    
+    printf("%d", visited[n-1][m-1]);
     return 0;
 }
