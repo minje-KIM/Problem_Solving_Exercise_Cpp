b72561160c28af6edd4f1a8b315296e26548e5eb
minje KIM <minje617@naver.com>
2025-01-24 21:42:18 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int num;
+
+int main ()
+{
+    
+    cin >> num;
+    
+    int j = ((num-1) * 2) + 1;
+    
+    for (int i=0; i<num; i++)
+    {
+        int mid = (j/2) + 1;
+        
+        for (int k=0; k<j; k++)
+        {
+            
+            int c = i;
+            int d = (j-1-i);
+            
+            if (k < c)
+                cout << " ";
+                
+            else if (k > d)
+                continue;
+            else
+                cout << "*";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
