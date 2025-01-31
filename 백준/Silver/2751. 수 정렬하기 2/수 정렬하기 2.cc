f426e4f3d852429e8aeab60a9954e205826608f9
minje KIM <minje617@naver.com>
2025-01-31 12:58:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 592 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int num;
+int temp;
+vector<int> n;
+
+
+int main ()
+{
+    cin >> num;
+    
+    for (int i=0; i<num; i++)
+    {
+        cin >> temp;
+        n.push_back(temp);
+    }
+    
+    
+    sort(n.begin(), n.end());
+    
+    for (auto i : n)
+        cout << i << "\n";
+    
+    return 0;
+}
