a95a0cd1b99783b29420342cd213b7d981afd7bf
minje KIM <minje617@naver.com>
2025-02-06 16:05:24 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isused[10];
+int num[10];
+int temp;
+
+void func(int k)
+{
+    if (k == m)
+    {
+        for (int i=0; i<m; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+    
+    for (int i=0; i<n; i++)
+    {
+
+        // if (k > 0 && arr[k-1] > i)
+        //     continue;
+
+
+        if(!isused[i])
+        {
+            arr[k] = num[i];
+            isused[i] = 1;
+            func(k+1);
+            isused[i] = 0;
+
+        }
+    }
+}
+
+int main ()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    
+    for (int i=0; i<n; i++)
+    {
+        cin >> num[i];
+    }
+    
+    sort(num, num+n);
+    
+    func(0);
+    
+    return 0;
+}
